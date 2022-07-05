a7396814a6ad44261c6e33bc165036e3490a27ab
Sumukh <sumukhagar041@gmail.com>
2022-07-05 19:48:55 +0530

    complete

--- sHome.php
+++ sHome.php
@@ -1,10 +1,19 @@
 <?php
+    session_start();
     if(isset($_GET['B1'])){
-        header("location:tLogin.php");
+        header("location:sUpdate.php");
     }else if(isset($_GET['B2'])){
-        header("location:sLogin.php");
+        header("location:sTimeTable.php");
     }else if(isset($_GET['B3'])){
-        header("location:aLogin.php");
+        header("location:sClass.php");
+    }else if(isset($_GET['B4'])){
+        header("location:sFeedback.php");
+    }else if(isset($_GET['B5'])){
+        header("location:sChangePassword.php");
+    }else if(isset($_GET['B6'])){
+        header("location:sLogout.php");
+    }else{
+        
     }
 ?>
 <html>
@@ -14,9 +23,19 @@
     <body><font color ="green">
         <h1 align="center">XYZ PUBLIC SCHOOL</h1><br></font>
         <form align="center" method="get">
-            <button type="submit" name="B1"> Teacher Portal </button><br><br>
-            <button type="submit" name="B2"> Student Portal </button><br><br>
-            <button type="submit" name="B3"> Admin Portal </button><br>
+            <table border="2" align="center">
+                <tr>
+                    <td align="center"><button type="submit" name="B1"> Update profile </button></td>
+                    <td align="center"><button type="submit" name="B2"> Show time table </button></td>
+                <tr>
+                <tr>
+                    <td align="center"><button type="submit" name="B3"> Class details </button></td>
+                    <td align="center"><button type="submit" name="B4"> Feedback </button></td>
+                <tr>
+                <tr>
+                    <td align="center"><button type="submit" name="B5"> Change password </button></td>
+                    <td align="center"><button type="submit" name="B6"> Logout </button></td>
+                <tr>
         </form>
     </body>
 </html>
