82a95b9364a08b423591d08e9c4d186c4a2c6c00
Sumukh <sumukhagar041@gmail.com>
2022-08-09 13:57:15 +0530

    complete

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         header("location:sLogin.php");
     }else if(isset($_GET['B3'])){
         header("location:aLogin.php");
+    }else if(isset($_GET['B4'])){
+        header("location:checkMail.php");
     }
 ?>
 <html>
@@ -17,6 +19,7 @@
             <button type="submit" name="B1"> Teacher Portal </button><br><br>
             <button type="submit" name="B2"> Student Portal </button><br><br>
             <button type="submit" name="B3"> Admin Portal </button><br>
+            <button type="submit" name="B4"> Check Mail </button><br>
         </form>
     </body>
 </html>
